719e3c27880c5db626ce211653bb1a1dc54a11c5
tortei1000 <tiagomaffei@gmail.com>
2019-04-08 21:44:45 -0600

    started with add

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -27,7 +27,20 @@ const recipes = [
    ],
    "image": "https://images.media-allrecipes.com/userphotos/250x250/733532.jpg",
     "directions": "Blend the melted chocolate and milk. Gradually mix in the water, vanilla, and pudding mix, and whisk until smooth. Fold in the heavy cream. Frost the cake immediately, and chill at least 30 minutes before serving."
-  }
+  },
+  {
+    "id": id++,
+    "name": "Fried egg",
+    "ingredient": [
+      {
+        "name": "egg",
+        "quantity": "1"
+      }
+    ],
+    "image":"https://www.bhg.com.au/cdnstorage/cache/b/b/d/c/b/0/xbbdcb0c3820b78995688479f1dcf81fa1c50dfaf.jpg.pagespeed.ic.XQMI5E9Num.webp",
+    "directions": "fry egg on a frying pan."
+  },
+
 ]
 
 module.exports = {
