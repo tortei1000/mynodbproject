c85335e1da4969f04b26ddecdfedc507c4dc2a8d
tiago maffei <tiagomaffei@gmail.com>
2019-04-11 14:24:17 -0600

    css

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -36,8 +36,9 @@ export default class Recipe extends Component {
           <p>{recipe.ingredient5}</p>
           <h3>Directions:</h3>
           <p>{recipe.directions}</p>
-          <button className="bottomButton" onClick={this.toggleEdit}>edit</button>
+          <div><button className="bottomButton" onClick={this.toggleEdit}>edit</button>
           <button className="bottomButton" onClick={() => this.props.removeRecipe(recipe)}>Delete</button>
+          </div>
           <button className="bottomButton2" onClick={()=> this.props.addItems([recipe.ingredient1,recipe.ingredient2,recipe.ingredient3,recipe.ingredient4,recipe.ingredient5])} >Add ingredients to shopping list</button>
 
         </div>
