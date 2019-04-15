15180420c0d99595786e904df133a5efb0dbf247
tiago maffei <tiagomaffei@gmail.com>
2019-04-15 11:17:25 -0600

    changed recipes

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,22 +1,16 @@
 let id = 1;
 const recipes = [
   {
-    "id" : id++,
-    "title": "Chocolate Cream Frosting",
-    "ingredient1": "2 ounces Chocolate",
-    "ingredient2": "14 ounces Sweetened condensed milk",  
-    "ingredient3": "1/2 teaspoonVanilla extract", 
-    "ingredient4": "4 ounces Instant chocolate pudding",
-    "ingredient5":"1 cup Heavy cream",
-    "imageUrl": "https://images.media-allrecipes.com/userphotos/250x250/733532.jpg",
-    "directions": "Blend the melted chocolate and milk. Gradually mix in the water, vanilla, and pudding mix, and whisk until smooth. Fold in the heavy cream. Frost the cake immediately, and chill at least 30 minutes before serving."
-  },
-  {
     "id": id++,
-    "title": "Fried egg",
-    "ingredient1": "1 egg",
-    "imageUrl":"https://www.bhg.com.au/cdnstorage/cache/b/b/d/c/b/0/xbbdcb0c3820b78995688479f1dcf81fa1c50dfaf.jpg.pagespeed.ic.XQMI5E9Num.webp",
-    "directions": "fry egg on a frying pan."
+    "title": "Mushroom Pork Chops",
+    "ingredient1": "4 pork chops",
+    "ingredient2": "salt and pepper",
+    "ingredient3": "1 pinch garlic salt",
+    "ingredient4": "1 onion chopped",
+    "ingredient5": "11 ounces cream of mushroom soup",
+    "imageUrl": "https://images.media-allrecipes.com/userphotos/560x315/714402.jpg",
+    "directions":"Season pork chops with salt, pepper, and garlic salt to taste. In a large skillet, brown the chops over medium-high heat. Add the onion and mushrooms, and saute for one minute. Pour cream of mushroom soup over chops. Cover skillet, and reduce temperature to medium-low. Simmer 20 to 30 minutes, or until chops are cooked through.",
+    
   },
   {
     "id": id++,
